e09ef8245aca2996ac35cc1833bc6bbf1148fb95
robmi11 <robert.mizianty@gmail.com>
2022-06-16 00:32:07 +0200

    Webpack config done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import generateJoke from "./generateJoke";
 import "./styles/main.scss";
+import logo from "./assets/logo.svg";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const joke = document.getElementById("joke");
+  const p = document.createElement("p");
+
+  const laughImg = document.getElementById("laughImg");
+  laughImg.src = logo;
+  p.innerText = generateJoke();
+
+  joke.appendChild(p);
+});
 
 console.log(generateJoke());
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -6,8 +7,20 @@ module.exports = {
     bundle: path.resolve(__dirname, "src/index.js"),
   },
   output: {
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    hot: true,
+    compress: true,
+    port: 5000,
+    open: true,
   },
   module: {
     rules: [
@@ -15,6 +28,28 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(svg|png|gif|jpg|jpeg|ico)$/i,
+        type: "asset/resource",
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My Webpack starter pack",
+      filename: "index.html",
+      template: "/home/robert/Web/Webpack/starter/src/templates/template.html",
+    }),
+  ],
 };
+// /home/robert/Web/Webpack/starter/src/templates/template.html
